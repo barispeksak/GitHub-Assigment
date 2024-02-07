b900bd8e9c2eeca03649f334682a37990e97d6b4
Barış Peksak <barispeksak@icloud.com>
2024-02-07 11:49:11 +0300

    Task D added

--- Operator.java
+++ Operator.java
@@ -68,5 +68,26 @@ public class Operator
         }
         return sumOfOdd;
     }
+
+    //Task D
+    public int[] differences(int[] numbers)
+    {
+        int total = 0;
+        for(int number : numbers)
+        {
+            total += number;
+        }
+        int avarage = total / numbers.length;
+
+        int[] newArray = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            newArray[i] = numbers[i] - avarage;
+        }
+
+        return newArray;
+    }
+
 }
 
